1a0aea91d1236c9d750ac9bea43bc323a46bd875
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-09-21 15:06:34 -0500

    quiz

---
+++ 9-21-quic/app.js
@@ -0,0 +1,9 @@
+//Create an object that has a property that is an array. Log one of the elements of that array.
+
+var objOne = { array: [1,2,3,4] };
+console.log(objOne.array[0]);
+//Create an object that has a property that is an object. Log one of the properties of that inner object.
+
+var objTwo = { obj: {inner: 'hello'}}
+console.log(objTwo.obj.inner);
+//Create an object that has a property that is a function (method). Call that method.
