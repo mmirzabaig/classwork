6a6d3726467a02bd33d3eb05ff2df28f6a5fca91
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-15 16:14:40 -0500

    index conencted

---
+++ two-models-lab-10-15/controllers/pC.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
+
+const Pets = require('../models/pets');
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
